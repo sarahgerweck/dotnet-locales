2189a04bd4f850292b8b6c8397b6cc9f7821c9ca
Sarah Gerweck <sarah.a180@gmail.com>
2018-11-19 17:30:54 -0800

    Simple program to print out all .NET locales

---
+++ README.md
@@ -0,0 +1,17 @@
+# Locales
+
+A simple program to list all of the .NET specific locales and print them out
+in a convenient format to bring into Scala (or another language).
+
+## Usage
+
+### Prereqs
+
+You'll need to install [.NET Core](https://www.microsoft.com/net/download)
+for your platform.
+
+### Running
+
+To run the app, just do
+
+    dotnet run
---
+++ locales.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace locales {
+    class Program {
+        static void Main(string[] args) {
+            var filtered =
+                from c in CultureInfo.GetCultures(CultureTypes.AllCultures)
+                where c.LCID != 0x1000 && c.LCID != 0x007F
+                orderby c.LCID
+                select c;
+
+            Console.WriteLine($"/* Found {filtered.Count()} .NET cultures */\n");
+            foreach (var culture in filtered) {
+                var cid = culture.LCID;
+                var ct = culture.IetfLanguageTag;
+                Console.WriteLine($"0x{cid:X4} -> \"{ct}\"");
+            }
+        }
+    }
+}
